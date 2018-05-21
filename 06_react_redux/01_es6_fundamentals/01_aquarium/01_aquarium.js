6a1c71cb2c157662539f186a847d804710ac50d0
Damien Desprez <damien.desprez@decathlon.com>
2018-05-21 18:28:49 +0200

    exercice

--- 06_react_redux/01_es6_fundamentals/01_aquarium/01_aquarium.js
+++ 06_react_redux/01_es6_fundamentals/01_aquarium/01_aquarium.js
@@ -1,9 +1,79 @@
 // Insert code here
+class Aquarium {
+  constructor(fishes,algaes){
+    this.fishes = fishes;
+    this.algaes = algaes;
+  }
+  addFish(fish){
+    this.fishes.push(fish);
+  }
+  addAlgae(algae){
+    this.algaes.push(algae);
+  }
+  endTurn(){
+    // console.log(this.algae.length);
+    // console.log(this.fish.length);
+    this.fishes.forEach(fish => this.timeToDinner(fish));
+  }
+  // veganFishesEatAlgae(){
+  //   if (this.algaes.length > 0){
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+  veganFishesEatAlgae(){
+    return (this.algaes.length > 0);
+  }
 
 
+  // carnivorFishesEatFish(){
+  //   if (this.fishes.length > 1) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+  carnivorFishesEatFish(){
+    return (this.fishes.length > 0);
+  }
+
+
+
+  timeToDinner(fish) {
+    const {isCarnivor} = fish;
+    if(isCarnivor === 0) {
+      if (this.carnivorFishesEatFish()) {
+        this.fishes.pop();
+      }
+    } else {
+      if (this.veganFishesEatAlgae()) {
+        this.algaes.pop();
+      }
+    }
+  }}
+
+
+class Fish {
+  constructor(name,sex, isCarnivor = 0){
+    this.name = name ;
+    this.sex = sex;
+    this.isCarnivor = isCarnivor;
+    //this.vegan = vegan dans ce cas, on ajoute vegan en parametre
+  }
+}
+
+class Algae {
+  constructor(size){
+    this.size = size;
+  }
+}
 
 module.exports = {
   aquarium: Aquarium,
   fish: Fish,
   algae: Algae
 };
+
+// const aqua = new Aquarium([new Fish("Nemo", "m")], [new Algae(14)]);
+// aqua.addFish(new Fish("toto", "m"));
--- 06_react_redux/01_es6_fundamentals/02_spread_destructuring/02_spread_destructuring.js
+++ 06_react_redux/01_es6_fundamentals/02_spread_destructuring/02_spread_destructuring.js
@@ -1,36 +1,53 @@
-function go(options) {
+//1er possibilité
+
+function go(options = {}) {
   let {
     speed = 4,
-    enable: { hyperdrive = false, frobnifier = true}
+    enable: { hyperdrive = false, frobnifier = true } = {}
   } = options;
 
   console.log("speed=", speed, "hyperdrive:", hyperdrive, "frobnifier:", frobnifier);
 }
-
+go();
+//2e possibilité
+function go(options) {
+  let {
+    speed = 4,
+    enable =  {hyperdrive : hyperdrive = false, frobnifier : frobnifier = true}
+  } = options;
+  console.log("speed=", speed, "hyperdrive:", hyperdrive, "frobnifier:", frobnifier);
+}
 go({ speed: 5 });
 
-function lastIndexOf(arr, elt, start) {
+/////////////////////////////////////////////////
+
+function lastIndexOf(arr, elt, start=arr.length) {
   for (let i = start - 1; i >= 0; i--) {
     if (arr[i] === elt) {
       return i;
     }
   }
-
   return -1;
 }
-
 console.log(lastIndexOf([1, 2, 1, 2], 2));
 
+
+/////////////////////////////////////////////////
+
 function replace(array, from, to, elements) {
-  array.splice.apply(array, [from, to - from].concat(elements));
+  //array.splice.apply(array, [from, to - from].concat(elements));
+  array.splice(from, to - from,...elements);
 }
 
 let testArray = [1, 2, 100, 100, 6];
 replace(testArray, 2, 4, [3, 4, 5]);
 console.log(testArray);
 
+
+
 function copyReplace(array, from, to, elements) {
-  return array.slice(0, from).concat(elements).concat(array.slice(to));
+  //return array.slice(0, from).concat(elements).concat(array.slice(to));
+  return[...array.slice(0, from),...elements,...(array.slice(to))];
 }
 
 console.log(copyReplace([1, 2, 100, 200, 6], 2, 4, [3, 4, 5]));
