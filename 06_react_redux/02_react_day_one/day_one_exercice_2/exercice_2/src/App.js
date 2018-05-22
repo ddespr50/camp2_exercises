abbb925046be8860d39e925c89de4a6d28a2c0d3
Damien Desprez <damien.desprez@decathlon.com>
2018-05-22 17:20:27 +0200

    exercice

---
+++ 06_react_redux/02_react_day_one/day_one_exercice_2/exercice_2/src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import logo from './logo.svg';
+import './App.css';
+import _ from "underscore";
+import products from './Products.js';
+
+class Productstable extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {products: products, sort: 'ASC'}
+  }
+
+  sortBy (column){
+    let sortedProducts = this.state.products;
+    if(this.state.sort === 'ASC'){
+      sortedProducts = _.sortBy(this.state.products, column);
+      this.setState({sort : 'DESC'});
+    }else{
+      sortedProducts = _.sortBy(this.state.products, column).reverse();
+      this.setState({sort : 'ASC'});
+    }
+      this.setState({products : sortedProducts})
+  }
+
+  render() {
+    return (
+      <div>
+      <table class="table table-striped table-secondary text-center">
+        <thead>
+          <tr>
+            <th scope="col" onClick={this.sortBy.bind(this, "decathlon_id")}>Decathlon_id</th>
+            <th scope="col" onClick={this.sortBy.bind(this, "title")}>Title</th>
+            <th scope="col" onClick={this.sortBy.bind(this, "price")}>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+        {this.state.products.map(product =>
+          <tr>
+            <td>{product.decathlon_id}</td>
+            <td>{product.title}</td>
+            <td>{product.price}</td>
+          </tr>
+        )}
+        </tbody>
+      </table>
+      </div>
+    );
+  }
+}
+
+export default Productstable;
