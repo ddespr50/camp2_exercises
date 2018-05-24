2a43345773f005799c9885dbf6f21e2f18cc4d82
Damien Desprez <damien.desprez@decathlon.com>
2018-05-24 17:27:40 +0200

    react

--- 06_react_redux/04_lifecycle_method/04_chatbot/src/ChatBot.js
+++ 06_react_redux/04_lifecycle_method/04_chatbot/src/ChatBot.js
@@ -3,24 +3,38 @@ import React, { Component } from "react";
 class ChatBot extends Component {
   constructor(props) {
     super(props);
+    this.refDom = React.createRef();
     this.sayHello = this.sayHello.bind(this);
     this.state = {
       messages: []
     };
   }
-  componentDidMount() {
-    setInterval(this.sayHello, 1000);
+
+  componentDidUpdate(){
+    this.refDom.current.scrollTop = this.refDom.current.scrollHeight;
   }
+
+  componentWillUnmount(){
+    clearInterval(this.state.linkInterval);
+  }
+
+  componentDidMount() {
+    const linkInterval = setInterval(this.sayHello, 100);
+    this.setState ({linkInterval} : linkInterval)
+  }
+
   sayHello() {
     this.setState({
       messages: [...this.state.messages, "Hey there!"]
     });
   }
   render() {
+    console.log("render")
     return (
-      <div>
+      <div ref={this.refDom} style={{height: 300, overflow:"auto"}}>
         {this.state.messages.map((message, index) =>
-          <div key={index}>{message}</div>
+          <div
+            key={index}>{message}</div>
         )}
       </div>
     )
