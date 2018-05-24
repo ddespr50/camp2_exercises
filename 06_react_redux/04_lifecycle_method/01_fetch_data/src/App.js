77fa775b25eef534133ad81f4f31dfc20e213df6
Damien Desprez <damien.desprez@decathlon.com>
2018-05-24 15:31:04 +0200

    exercice done

--- 06_react_redux/04_lifecycle_method/01_fetch_data/src/App.js
+++ 06_react_redux/04_lifecycle_method/01_fetch_data/src/App.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class DogApp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      url: ""
+    };
+  }
+
+  componentDidMount() {
+    this.refresh();
+  }
+
+  refresh(){
+    console.log("refresh");
+    return fetch('https://random.dog/woof.json',
+    {method : 'get'})
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          url: data.url
+        });
+      })
+  }
+
   render() {
     return (
-      <div className="App">
-
+      <div>
+        <div className="App">
+          <div> Bonjour , le chien </div>
+          <img src= {this.state.url} />
+        </div>
+        <button onClick={() => this.refresh()}>Refresh</button>
       </div>
     );
   }
--- 06_react_redux/04_lifecycle_method/02_transform_data/src/App.js
+++ 06_react_redux/04_lifecycle_method/02_transform_data/src/App.js
@@ -1,16 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import expenses from './expenses';
+import users from './users';
+import _ from 'underscore';
+
 
 // App has `expenses` and `users` as props
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      expenses: expenses,
+      users: users
+    };
+  }
+
+
   render() {
     return (
       <div className="App">
-
+        <table class="table table-striped table-secondary text-center">
+          <thead>
+            <tr>
+              <th scope="col" >What has been paid</th>
+              <th scope="col" >By whom?</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.expenses.map((expense,index) => {
+              const userFound = _.findWhere(this.state.users, {id: expense.who});
+              let username="";
+              if (userFound !== undefined){
+                username = userFound.name;
+              }
+              return (
+                <tr>
+                  <td>{expense.what}</td>
+                  <td>{username}</td>
+                </tr>
+            )
+          }
+          )
+        }
+          </tbody>
+        </table>
       </div>
-    );
-  }
+    )}
 }
+
+
+
 
 export default App;
